6ea55ee60e21e94f58835b70064ceee7aad972f3
chaoyuxie <1596580771@qq.com>
2018-05-15 10:31:19 +0800

    Search Insert Position

--- 035.cpp
+++ 035.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index = 0;
+        for(const auto& num:nums)
+        {
+            if(num < target)
+            {
+                index++;
+            }
+        }
+        return index;
+    }
+};
