01905144034605d4797a8e94931c5decb9fdc314
chaoyuxie <1596580771@qq.com>
2018-05-15 11:47:59 +0800

    Maximum Subarray

--- 053.cpp
+++ 053.cpp
@@ -1,12 +1,14 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
+class Solution
+{
+  public:
+    int maxSubArray(vector<int> &nums)
+    {
         int totalsum = nums[0];
         int currentsum = nums[0];
-        for(int i = 1; i < nums.size(); i++)
+        for (int i = 1; i < nums.size(); i++)
         {
-            currentsum = max(nums[i],nums[i]+currentsum);
-            totalsum = max(currentsum,totalsum);
+            currentsum = max(nums[i], nums[i] + currentsum);
+            totalsum = max(currentsum, totalsum);
         }
         return totalsum;
     }
