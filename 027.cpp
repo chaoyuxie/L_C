0769f4669178277f113e0ec8b8123bb31bbc6eda
chaoyuxie <1596580771@qq.com>
2018-05-15 11:47:00 +0800

    Remove Element

--- 027.cpp
+++ 027.cpp
@@ -1,10 +1,12 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
+class Solution
+{
+  public:
+    int removeElement(vector<int> &nums, int val)
+    {
         int index = 0;
-        for(const auto& num:nums)
+        for (const auto &num : nums)
         {
-            if(num != val)
+            if (num != val)
             {
                 nums[index++] = num;
             }
