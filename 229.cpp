1f5f73f93f00008cefaf6627ec62341d397d5863
chaoyuxie <31816849+chaoyuxie@users.noreply.github.com>
2018-05-16 22:01:25 +0800

    Majority Element II

---
+++ 229.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        map<int, int> numbers;
+        vector<int> result;
+        for(const auto& num:nums)
+        {
+            ++numbers[num];
+        }
+        for(const auto& num:numbers)
+        {
+            if(num.second > nums.size()/3)result.push_back(num.first);
+        }
+        return result;
+    }
+};
