77ea2489e620fa026b0039ad0888a7b6a2f47573
chaoyuxie <31816849+chaoyuxie@users.noreply.github.com>
2018-05-16 09:53:41 +0800

    Pascal's Triangle II

---
+++ 119.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+  public:
+    vector<int> getRow(int k)
+    {
+        vector<int> ans(k + 1, 1);
+        for (int i = 1; i <= k / 2; ++i)
+        {
+            ans[k - i] = ans[i] = long(ans[i - 1]) * (k - i + 1) / i;
+        }
+        return ans;
+    }
+};
