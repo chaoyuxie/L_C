1e3a90fb6af62ddad6caa1455d671cc2cdedbf38
chaoyuxie <31816849+chaoyuxie@users.noreply.github.com>
2018-05-27 00:05:16 +0800

    Remove Element

--- py/027.py
+++ py/027.py
@@ -6,9 +6,9 @@ class Solution:
         :rtype: int
         """
         i, result = 0, len(nums)-1
-        while i<=result:
+        while i <= result:
             if nums[i] == val:
-                nums[i],nums[result] = nums[result],nums[i]
+                nums[i], nums[result] = nums[result], nums[i]
                 result -= 1
             else:
                 i += 1
