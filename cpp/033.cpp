3fbc078a3c9e6b2e4152ce7e58543b02ef7910f2
chaoyuxie <1596580771@qq.com>
2018-07-21 14:46:20 +0800

    Search in Rotated Sorted Array

--- cpp/033.cpp
+++ cpp/033.cpp
@@ -1,32 +1,36 @@
-class Solution(){
-    public :
-        int search(vector<int> & nums, target){
-            int left = 0;
-int right = nums.size() - 1;
-while (left <= right)
-{
-    int mid = left + (right - left) / 2;
-    if (nums[mid] == target)
-        return mid;
-    else if (nums[right] < nums[mid])
-    {
-        if (target >= nums[left] && target < nums[mid])
-            right = mid - 1;
-        else
-            left = mid + 1;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()) {
+            return -1;
+        }
+        
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (target == nums[mid]) {
+                return mid;
+            } else if (nums[high] < nums[mid]) {  // 左侧有序
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {  // 右侧有序
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
     }
-    else
-    {
-        if (target > nums[mid] && target <= nums[high])
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-}
-return -1;
-}
-}
-;
+};
 
 const auto ___ = []() {
     std::ios::sync_with_stdio(false);
