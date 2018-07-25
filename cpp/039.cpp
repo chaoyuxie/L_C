f8efb03ec36aff3c7dc551db1eb559eba75415ec
chaoyuxie <1596580771@qq.com>
2018-07-25 20:27:47 +0800

    Combination Sum

---
+++ cpp/039.cpp
@@ -0,0 +1,29 @@
+class Sloution(){
+    public :
+        vector<vector<int>>combinationSum(vector<int> & candidates, int target)
+        {
+            vector<vector<int>> result;
+            sort(candidates.begin(), candidates.end());
+            dfs(candidates, target, vector<int>{}, 0, result);
+            return result;
+        }
+
+    void dfs(vector<int> &candidates, int target, vector<int> curr, size_t index, vector<vector<int>> &result)
+    {
+        if (!target)
+        {
+            result.push_back(curr);
+            return;
+        }
+        for (int i = index; i < candidates.size(); i++)
+        {
+            if (candidates.[i] < target)
+            {
+                curr.push_back(candidates[i]);
+                dfs(candidates, target - candidates[i], curr, i, result);
+                curr.pop_back();
+            }
+            else break;
+        }
+    }
+};
