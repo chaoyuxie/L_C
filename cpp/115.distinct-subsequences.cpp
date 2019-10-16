da2e1dbcd257f2b72823c648d7148a6415b564b6
chaoyuxie <1596580771@qq.com>
2019-10-16 15:19:48 +0800

    solution

---
+++ cpp/115.distinct-subsequences.cpp
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=115 lang=cpp
+ *
+ * [115] Distinct Subsequences
+ *
+ * https://leetcode.com/problems/distinct-subsequences/description/
+ *
+ * algorithms
+ * Hard (35.96%)
+ * Likes:    811
+ * Dislikes: 43
+ * Total Accepted:    115.8K
+ * Total Submissions: 321.9K
+ * Testcase Example:  '"rabbbit"\n"rabbit"'
+ *
+ * Given a string S and a string T, count the number of distinct subsequences
+ * of S which equals T.
+ * 
+ * A subsequence of a string is a new string which is formed from the original
+ * string by deleting some (can be none) of the characters without disturbing
+ * the relative positions of the remaining characters. (ie, "ACE" is a
+ * subsequence of "ABCDE" while "AEC" is not).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: S = "rabbbit", T = "rabbit"
+ * Output: 3
+ * Explanation:
+ * 
+ * As shown below, there are 3 ways you can generate "rabbit" from S.
+ * (The caret symbol ^ means the chosen letters)
+ * 
+ * rabbbit
+ * ^^^^ ^^
+ * rabbbit
+ * ^^ ^^^^
+ * rabbbit
+ * ^^^ ^^^
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: S = "babgbag", T = "bag"
+ * Output: 5
+ * Explanation:
+ * 
+ * As shown below, there are 5 ways you can generate "bag" from S.
+ * (The caret symbol ^ means the chosen letters)
+ * 
+ * babgbag
+ * ^^ ^
+ * babgbag
+ * ^^    ^
+ * babgbag
+ * ^    ^^
+ * babgbag
+ * ⁠ ^  ^^
+ * babgbag
+ * ⁠   ^^^
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = t.size(),m = s.size(); 
+        vector<vector<long>> dp(n + 1, vector<long>(m + 1));
+        for (int j = 0; j <= m; ++j) dp[0][j] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+            }
+        }
+        return dp[n][m];
+    }
+};
+// @lc code=end
+
---
+++ cpp/97.interleaving-string.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=97 lang=cpp
+ *
+ * [97] Interleaving String
+ *
+ * https://leetcode.com/problems/interleaving-string/description/
+ *
+ * algorithms
+ * Hard (29.15%)
+ * Likes:    948
+ * Dislikes: 53
+ * Total Accepted:    124.8K
+ * Total Submissions: 427.8K
+ * Testcase Example:  '"aabcc"\n"dbbca"\n"aadbbcbcac"'
+ *
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and
+ * s2.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * Output: false
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        
+    }
+};
+// @lc code=end
+
